6d3921c79863bc2d0cd29864f0f38015f1fd35d8
May Yong <myong@turing.ac.uk>
2025-04-28 16:03:02 +0100

    Manual approach

--- dummy_data/basic/bin/main.ml
+++ dummy_data/basic/bin/main.ml
@@ -15,28 +15,26 @@ let print_patient (p:patient): unit =
     Printf.printf "ID:%i \tsex:%s \tdob:%s \tdate_of_referral:%s age: %i age_at_referral: %i\n" 
         p.id (string_of_sex p.sex) (print_date p.date_of_birth) (print_date p.date_of_referral) age age_at_referral
 
-let generate_date (years:int) : Unix.tm = 
+let generate_date ~(years_since_1900:int) : Unix.tm = 
     {
         Unix.tm_sec = 0;
         tm_min = 0;
         tm_hour = 0;
         tm_mday = 1;
         tm_mon = 0;    (* January = 0 *)
-        tm_year = years; (* 2000 - 1900 = 100 *)
+        tm_year = years_since_1900; (* 2000 - 1900 = 100 *)
         tm_wday = 0; tm_yday = 0; tm_isdst = false
     } 
 
 let generate_patients (n:int) : patient list =
-    (* let study_start_date = generate_date 25 in
-    let study_end_date = generate_date 50 in *)
-    let youngest_dob = generate_date 100 in
-    let oldest_dob = generate_date 60 in
+    let oldest_dob = generate_date ~years_since_1900:60 in
+    let youngest_dob = generate_date ~years_since_1900:100 in
     List.init n (fun i -> 
         let sex = sex_of_index (random_int ~min:0 ~max:count_sexes) in
         let date_of_birth = random_date ~start:oldest_dob ~end_:youngest_dob in
         let age = float_of_int @@ years_delta date_of_birth current_date in
         let sample_age_referral = gaussian ~mu:age ~sigma:(age /. 2.) in
-        let date_of_referral = advance_date date_of_birth (int_of_float @@ sample_age_referral ()) in
+        let date_of_referral = next_date date_of_birth (int_of_float @@ sample_age_referral ()) in
         generate_patient (i+1) date_of_birth sex date_of_referral
     )
 
--- dummy_data/basic/bin/utilities.ml
+++ dummy_data/basic/bin/utilities.ml
@@ -1,12 +1,34 @@
 
+(** [print_date tm] formats a [Unix.tm] record into a string representation 
+    in the format "YYYY-MM-DD".
+
+    @param tm The date to format as a [Unix.tm] record.
+    @return A string representing the formatted date.
+*)
 let print_date (tm:Unix.tm):string =
   Printf.sprintf "%04d-%02d-%02d" (tm.tm_year + 1900) (tm.tm_mon + 1) tm.tm_mday
+
+(** [random_int ~min ~max] generates a random integer within the inclusive range 
+    between [min] and [max].
+
+    @param min The minimum value of the range.
+    @param max The maximum value of the range.
+    @return A random integer within the specified range.
+*)
 
 let random_int ~(min:int) ~(max:int):int =
   let range = max - min + 1 in
   Random.int range + min
 
-let gaussian ~mu ~sigma =
+(** [gaussian ~mu ~sigma] generates a random number following a Gaussian 
+    (normal) distribution with mean [mu] and standard deviation [sigma]. 
+    The result is constrained to the range [0.0, 100.0].
+
+    @param mu The mean of the distribution.
+    @param sigma The standard deviation of the distribution.
+    @return A random number following the specified Gaussian distribution.
+*)
+  let gaussian ~mu ~sigma =
   let rec sample () =
     let u1 = Random.float 1.0 in
     let u2 = Random.float 1.0 in
@@ -16,6 +38,13 @@ let gaussian ~mu ~sigma =
   in
   sample
 
+(** [random_date ~start ~end_] generates a random date between the given 
+    [start] and [end_] dates.
+
+    @param start The starting date as a [Unix.tm] record.
+    @param end_ The ending date as a [Unix.tm] record.
+    @return A random date as a [Unix.tm] record within the specified range.
+*)
 let random_date ~(start:Unix.tm) ~(end_:Unix.tm) =
   let start_time = Unix.mktime start |> fst |> int_of_float in
   let end_time = Unix.mktime end_ |> fst |> int_of_float in
@@ -28,6 +57,14 @@ let random_date ~(start:Unix.tm) ~(end_:Unix.tm) =
   Printf.printf "Random timestamp: %s\n" (print_date (Unix.gmtime random_timestamp));
   Unix.gmtime random_timestamp
 
+(** [years_delta start end_] calculates the difference in years between 
+    two dates, accounting for whether the end date has passed the 
+    anniversary of the start date.
+
+    @param start The starting date as a [Unix.tm] record.
+    @param end_ The ending date as a [Unix.tm] record.
+    @return The number of full years between the two dates.
+*)  
 let years_delta (start:Unix.tm)(end_:Unix.tm):int =
   let years = end_.tm_year - start.tm_year in
   if end_.tm_mon < start.tm_mon || (end_.tm_mon = start.tm_mon && end_.tm_mday < start.tm_mday) then
@@ -35,8 +72,35 @@ let years_delta (start:Unix.tm)(end_:Unix.tm):int =
   else
     years
 
-let advance_date (tm:Unix.tm) (years:int):Unix.tm =
-  let new_time = {tm with Unix.tm_year = tm.tm_year + years} in
+(** [advance_date start years] takes a date represented as a [Unix.tm] record 
+    and advances it by the specified number of years. It adjusts the resulting 
+    date to account for edge cases such as leap years or invalid dates.
+
+    @param start The starting date as a [Unix.tm] record.
+    @param years The number of years to advance the date by.
+    @return A [Unix.tm] record representing the adjusted date.
+*)
+let advance_date (start:Unix.tm) (years:int):Unix.tm =
+  let new_time = {start with Unix.tm_year = start.tm_year + years} in
   let _, corrected_tm = Unix.mktime new_time in
   corrected_tm
+
+(** [next_date start years] calculates the next date by advancing the given 
+    [start] date by the specified number of years. If the resulting date is 
+    in the future, it generates a random date between the start date and 
+    the current date.
+
+    @param start The starting date as a [Unix.tm] record.
+    @param years The number of years to advance the date by.
+    @return A [Unix.tm] record representing the next date.
+*)
+let next_date (start:Unix.tm) (years:int): Unix.tm =
+  let new_date, _ = Unix.mktime @@ advance_date start years in
+  let current_date = Unix.time () in
+  
+  if new_date > current_date then
+    random_date ~start:start ~end_:(Unix.gmtime current_date)
+  else
+    Unix.gmtime new_date
+
 
